961fc755c66bfb3ae1903b59e2e7d0832edeebef
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2021-09-21 20:48:50 -0300

    converte arquivo Gol em prolog

--- Prolog/Utils.pl
+++ Prolog/Utils.pl
@@ -4,6 +4,12 @@ replicate(X, N, L) :-
     length(L, N),
     maplist(=(X), L).
 
+range(A, A, []).
+range(A, B, R):-
+    NA is A + 1, 
+    range(NA, B, Rest), 
+    append([A], Rest, R).
+
 create_string(X, N, Str):- 
     replicate(X, N, L),
     atomic_list_concat(L, Str).
