730673b17d57b3f6de7e84452fa14657ed3c3cb6
Luan Carvalho Pedrosa <luan.pedrosa@ccc.ufcg.edu.br>
2021-09-28 07:40:58 -0300

    adicao do Patterns of Rules

---
+++ Prolog/PatternsofRules.pl
@@ -0,0 +1,163 @@
+:- include('Screen.pl').
+
+
+
+first([[1]],[[0]]).
+
+second([[0,0,0,0,0],
+          [0,1,0,1,0],
+          [0,0,1,0,0],
+          [0,1,0,1,0],
+          [0,0,0,0,0]],
+          [[0,0,0,0,0],
+          [0,0,1,0,0],
+          [0,1,0,1,0],
+          [0,0,1,0,0],
+          [0,0,0,0,0]]).
+
+
+third([[0,0,0,0,0],
+         [0,0,0,1,0],
+         [0,0,1,0,0],
+         [0,0,0,1,0],
+         [0,0,0,0,0]],
+         [[0,0,0,0,0],
+         [0,0,0,0,0],
+         [0,0,1,1,0],
+         [0,0,0,0,0],
+         [0,0,0,0,0]]).
+
+fourth([[0,0,0,0,0],
+          [0,1,0,0,0],
+          [0,0,1,0,0],
+          [0,0,0,1,0],
+          [0,0,0,0,0]],
+          [[0,0,0,0,0],
+          [0,0,0,0,0],
+          [0,0,1,0,0],
+          [0,0,0,0,0],
+          [0,0,0,0,0]]).
+
+
+firstExplanation(["Note que a célula da primeira geração deixa de estar viva na segunda, cumprindo com a regra."]).
+
+secondExplanation( ["Note que a célula que se encontra no meio do padrão na primeira geração, deixa de estar viva na segunda, cumprindo com a regra."]).
+
+thirdExplanation( ["Note que a célula não-viva que se encontra no meio das trẽs vivas na primeira geração, torna-se uma célula viva, cumprindo com a regra."]
+).
+
+fourthExplanation(["Note que a célula viva que se encontra no meio do padrão da primeira geração, continua na segunda, cumprindo com a regra."]).
+
+tag(["Primeira Geração:"],
+       ["Segunda Geração:"]).
+
+comandsTable( [
+    " q - Voltar"]).
+
+printRuleOne(_):-
+first(fst),
+first(snd),
+tag(T),
+firstExplanation(forE),
+commandsTable(CT),
+createScreenBuffer(width, height, emptyPxl, initialBuffer ),
+createBufferFromStringMatrix(CT, tableBuf),
+matrixToBuffer(fst, solidPxl, matrixBuf),
+matrixToBuffer(snd, solidPxl, sndMatrixBuf),
+createBufferFromStringMatrix(forE, explanationBuf),
+createBufferFromStringMatrix(T, fstText),
+createBufferFromStringMatrix(T, sndText),
+
+renderInBuffer(initialBuffer, tableBuf, 1, 3, tmp1),
+renderInBuffer(tmp1, matrixBuf, 30, 10, tmp2),
+renderInBuffer(tmp2, explanationBuf, 10, 30, tmp3),
+renderInBuffer(tmp3, sndMatrixBuf, 30, 20, tmp4),
+renderInBuffer(tmp4, fstText, 5, 10, tmp5),
+renderInBuffer(tmp5, sndText, 5, 20, tmp6),
+printScreen(tmp6).
+
+
+printRuleTwo(_):-
+second(fst),
+second(snd),
+tag(T),
+secondExplanation(forE),
+commandsTable(CT),
+createScreenBuffer(width, height, emptyPxl, initialBuffer ),
+createBufferFromStringMatrix(CT, tableBuf),
+matrixToBuffer(fst, solidPxl, matrixBuf),
+matrixToBuffer(snd, solidPxl, sndMatrixBuf),
+createBufferFromStringMatrix(forE, explanationBuf),
+createBufferFromStringMatrix(T, fstText),
+createBufferFromStringMatrix(T, sndText),
+
+renderInBuffer(initialBuffer, tableBuf, 1, 3, tmp1),
+renderInBuffer(tmp1, matrixBuf, 30, 10, tmp2),
+renderInBuffer(tmp2, explanationBuf, 4, 30, tmp3),
+renderInBuffer(tmp3, sndMatrixBuf, 30, 20, tmp4),
+renderInBuffer(tmp4, fstText, 5, 10, tmp5),
+renderInBuffer(tmp5, sndText, 5, 20, tmp6),
+printScreen(tmp6).
+
+
+printRuleThree(_):-
+third(fst),
+third(snd),
+tag(T),
+thirdExplanation(forE),
+commandsTable(CT),
+createScreenBuffer(width, height, emptyPxl, initialBuffer ),
+createBufferFromStringMatrix(CT, tableBuf),
+matrixToBuffer(fst, solidPxl, matrixBuf),
+matrixToBuffer(snd, solidPxl, sndMatrixBuf),
+createBufferFromStringMatrix(forE, explanationBuf),
+createBufferFromStringMatrix(T, fstText),
+createBufferFromStringMatrix(T, sndText),
+
+renderInBuffer(initialBuffer, tableBuf, 1, 3, tmp1),
+renderInBuffer(tmp1, matrixBuf, 30, 10, tmp2),
+renderInBuffer(tmp2, explanationBuf, 2, 30, tmp3),
+renderInBuffer(tmp3, sndMatrixBuf, 30, 20, tmp4),
+renderInBuffer(tmp4, fstText, 5, 10, tmp5),
+renderInBuffer(tmp5, sndText, 5, 20, tmp6),
+printScreen(tmp6).
+
+printRuleFour(_):-
+fourth(fst),
+fourth(snd),
+tag(T),
+fourthExplanation(forE),
+commandsTable(CT),
+createScreenBuffer(width, height, emptyPxl, initialBuffer ),
+createBufferFromStringMatrix(CT, tableBuf),
+matrixToBuffer(fst, solidPxl, matrixBuf),
+matrixToBuffer(snd, solidPxl, sndMatrixBuf),
+createBufferFromStringMatrix(forE, explanationBuf),
+createBufferFromStringMatrix(T, fstText),
+createBufferFromStringMatrix(T, sndText),
+
+renderInBuffer(initialBuffer, tableBuf, 1, 3, tmp1),
+renderInBuffer(tmp1, matrixBuf, 30, 10, tmp2),
+renderInBuffer(tmp2, explanationBuf, 5, 30, tmp3),
+renderInBuffer(tmp3, sndMatrixBuf, 30, 20, tmp4),
+renderInBuffer(tmp4, fstText, 5, 10, tmp5),
+renderInBuffer(tmp5, sndText, 5, 20, tmp6),
+printScreen(tmp6).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Prolog/Rules.pl
@@ -0,0 +1,10 @@
+menu( [
+    "1. Qualquer célula viva com menos de dois vizinhos vivos morre de solidão             ",
+    "2. Qualquer célula viva com mais de três vizinhos vivos morre de superpopulação       ",
+    "3. Qualquer célula com exatamente três vizinhos vivos se torna uma célula viva        ",
+    "4. Qualquer célula com dois vizinhos vivos continua no mesmo estado na próxima geração"]).
+
+commandsTable([
+    " s / w - mover cursor  ", 
+    " f - selecionar        ",
+    " q - Voltar"]).
